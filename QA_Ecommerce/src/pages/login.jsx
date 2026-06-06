bf92c1ab951e64e0911019108416ff2591904676
ABDULQANI AHMED <153436304+QaniAhmed@users.noreply.github.com>
2026-06-06 16:37:14 +0300

    Fix CSS import case in login.jsx

--- QA_Ecommerce/src/pages/login.jsx
+++ QA_Ecommerce/src/pages/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './login.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
